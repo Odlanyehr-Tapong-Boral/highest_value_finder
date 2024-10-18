e45087acbe5f5040b285f8a2aa29d6b3e7d2a0fb
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-18 21:47:48 +0800

    part13

--- value_finder.py
+++ value_finder.py
@@ -9,7 +9,6 @@ var5 = (input("please input No. here "))
 if var1 >= var2 and var1 >= var3 and var1 >= var4 and var1 >= var5 :
     highest = var1
 
-
 elif var2 >= var3 and var2 >= var4 and var2 >= var5 : 
     highest = var2
          
@@ -19,15 +18,20 @@ elif var3 >= var4 and var3 >= var5:
       
 
 elif var4 >= var5:
-    highest = var4
+    highest = var4 
+    
+else:
+    highest = var5
+    
+    
+print("The highest number is", highest)    
     
       
 
-else:
-    highest = var5   
+
     
     
-print(f"The highest Number is {highest}")    
+  
         
         
 
