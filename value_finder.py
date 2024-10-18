1d745dd0e39a5e3d3245bae546dd4e1bea42693d
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-18 17:38:25 +0800

    part4

--- value_finder.py
+++ value_finder.py
@@ -5,3 +5,6 @@ var3 = (input("please input No. here "))
 var4 = (input("please input No. here "))
 var5 = (input("please input No. here "))
 # Find and print the highest number using only if statement. 
+if var1 < var2 and var3 and var4 and var5:
+    print({var1})   
+    
