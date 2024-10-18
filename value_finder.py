5941d313d2055f1ab7ca68ba7f95f61895713745
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-18 22:04:23 +0800

    final_part

--- value_finder.py
+++ value_finder.py
@@ -1,27 +1,29 @@
 #Create a program that ask user to input 5 numbers. 
-var1 = (input("please input No. here "))
-var2 = (input("please input No. here "))
-var3 = (input("please input No. here "))
-var4 = (input("please input No. here "))
-var5 = (input("please input No. here "))
+var1 = (input("please input first number here ="))
+var2 = (input("please input second number here ="))
+var3 = (input("please input third number here ="))
+var4 = (input("please input fourth number here ="))
+var5 = (input("please input fifth number here ="))
 
 # Find and print the highest number using only if statement. 
-if var1 >= var2 and var1 >= var3 and var1 >= var4 and var1 >= var5 :
+if var1 >= var1 and var1 >= var2 and var1 >= var3 and var1 >= var4 and var1 >= var5 :
     highest = var1
 
-elif var2 >= var3 and var2 >= var4 and var2 >= var5 : 
+elif var2 >= var1 and var2 >= var2 and var2 >= var3 and var2 >= var4 and var2 >= var5 : 
     highest = var2
          
 
-elif var3 >= var4 and var3 >= var5:
+elif var3 >= var1 and var3 >= var2 and var3 >= var3 and var3 >= var4 and var3 >= var5 :
     highest = var3
       
 
-elif var4 >= var5:
+elif var4 >= var1 and var4 >= var2 and var4 >= var3 and var4 >= var4 and var4 >= var5 :
     highest = var4 
     
-else:
-    highest = var5
+elif var5 >= var1 and var5 >= var2 and var5 >= var3 and var5 >= var4 and var5 >= var5 :
+    highest = var5    
+    
+
     
     
 print("The highest number is", highest)    
