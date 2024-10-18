e0382c9ba0e12833bd4463de46c972d93619c651
Odlanyehr Boral <OdlanyehrBoral@gmail.com>
2024-10-18 19:57:18 +0800

    part12

--- value_finder.py
+++ value_finder.py
@@ -6,24 +6,28 @@ var4 = (input("please input No. here "))
 var5 = (input("please input No. here "))
 
 # Find and print the highest number using only if statement. 
-if var1>=var2 and var1>=var3 and var1>=var4 and var1>=var5 :
-    print(f"The highest is {var1}")
+if var1 >= var2 and var1 >= var3 and var1 >= var4 and var1 >= var5 :
+    highest = var1
 
 
-elif var2>= var3 and var2>=var4 and var1>=var5 : 
-        print(f"The highest {var2}")
+elif var2 >= var3 and var2 >= var4 and var2 >= var5 : 
+    highest = var2
          
 
-elif var3>= var4 and var3>=var5:
-        print(f"The highest {var3}")
+elif var3 >= var4 and var3 >= var5:
+    highest = var3
       
 
-elif var4>= var5:
-        print(f"The highest {var4}")
+elif var4 >= var5:
+    highest = var4
+    
       
 
 else:
-        print(f"The highest {var5}")
+    highest = var5   
+    
+    
+print(f"The highest Number is {highest}")    
         
         
 
